24da30704a2f30296bcb816aa1a37191743ca7d1
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2020-11-13 19:54:16 +0100

    Add workspace_analysis node
    Signed-off-by: Robert Wilbrandt <robert@stamm-wilbrandt.de>

--- src/workspace_analysis.cpp
+++ src/workspace_analysis.cpp
@@ -1,10 +1,25 @@
-#include <cstdio>
+/*
+ * Copyright (c) 2020 Robert Wilbrandt
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <cstdlib>
+#include <rclcpp/rclcpp.hpp>
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+class WorkspaceAnalysisNode : public rclcpp::Node {
+ public:
+  WorkspaceAnalysisNode() : Node("workspace_analysis") {
+    RCLCPP_INFO(this->get_logger(), "Started workspace analysis");
+  }
+};
 
-  printf("hello world workspace_analysis package\n");
-  return 0;
+int main(int argc, char** argv) {
+  (void)argc;
+  (void)argv;
+
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<WorkspaceAnalysisNode>());
+  rclcpp::shutdown();
+
+  return EXIT_SUCCESS;
 }
